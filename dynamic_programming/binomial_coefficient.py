6cbcc6177cddd59553ec650ce4f4b2c8419ccfb3
Hedie Hamdamian <165987346+Hediehamdam@users.noreply.github.com>
2026-07-13 10:14:19 +0330

    Create binomial_coefficient.py

---
+++ dynamic_programming/binomial_coefficient.py
@@ -0,0 +1,27 @@
+# Algorithm: Binomial Coefficient
+# Time Complexity: O(n*k)
+# Space Complexity: O(n*k)
+
+def binomial(n, k):
+    B = [[0] * (k + 1) for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for j in range(min(i, k) + 1):
+
+            if j == 0 or j == i:
+                B[i][j] = 1
+
+            else:
+                B[i][j] = (
+                    B[i - 1][j - 1]
+                    + B[i - 1][j]
+                )
+
+    return B[n][k]
+
+
+if __name__ == "__main__":
+    n = int(input("n = "))
+    k = int(input("k = "))
+
+    print("C(n,k) =", binomial(n, k))
